3838a46dc274cd50470408b18834a11524903d4b
Anestis Finstad <munkeyten@protonmail.com>
2020-06-01 00:00:14 +0200

    update data lint

--- src/data/data.js
+++ src/data/data.js
@@ -1,81 +1,81 @@
 const data = {
     json1: {
-        "psu1": {
+        psu1: {
             "status": "ok",
             "volt": "123"
         },
-        "psu2": {
+        psu2: {
             "status": "no",
             "volt": "Power Supply AC lost"
         },
-        "mgmt1": {
+        mgmt1: {
             "link_status": "no link",
             "bps": 0
         },
-        "mgmt2": {
+        mgmt2: {
             "link_status": "1000Mbit",
             "bps": 92931
         },
-        "data1": {
+        data1: {
             "link_status": "100000Mbit",
             "bps": 1234567
         },
-        "data2": {
+        data2: {
             "link_status": "100000Mbit",
             "bps": 4565645
         }
     },
     json2: {
-        "psu1": {
+        psu1: {
             "status": "no",
             "volt": "Power Supply AC Lost"
         },
-        "psu2": {
+        psu2: {
             "status": "ok",
             "volt": 231
         },
-        "mgmt1": {
+        mgmt1: {
             "link_status": "no link",
             "bps": 149412
         },
-        "mgmt2": {
+        mgmt2: {
             "link_status": "1000Mbit",
             "bps": 149412
         },
-        "data1": {
+        data1: {
             "link_status": "100000Mbit",
             "bps": 123567
         },
-        "data2": {
+        data2: {
             "link_status": "100000Mbit",
             "bps": 455645
         }
     },
     json3: {
-            "psu1": {
-                "status": "ok",
-                "volt": "233"
-            },
-            "psu2": {
-                "status": "ok",
-                "volt": "233"
-            },
-            "mgmt1": {
-                "link_status": "no link",
-                "bps": 0
-            },
-            "mgmt2": {
-                "link_status": "1000Mbit",
-                "bps": 12984
-            },
-            "data1": {
-                "link_status": "100000Mbit",
-                "bps": 1003567
-            },
-            "data2": {
-                "link_status": "100000Mbit",
-                "bps": 4055645
-            }
+        psu1: {
+            "status": "ok",
+            "volt": "233"
+        },
+        psu2: {
+            "status": "ok",
+            "volt": "233"
+        },
+        mgmt1: {
+            "link_status": "no link",
+            "bps": 0
+        },
+        mgmt2: {
+            "link_status": "1000Mbit",
+            "bps": 12984
+        },
+        data1: {
+            "link_status": "100000Mbit",
+            "bps": 1003567
+        },
+        data2: {
+            "link_status": "100000Mbit",
+            "bps": 4055645
+        }
     }
  }
 
