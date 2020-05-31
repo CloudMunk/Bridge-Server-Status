fa14d1eaeb73ef867b09aef63b05cdf5ce9d768f
Anestis Finstad <munkeyten@protonmail.com>
2020-05-31 23:23:25 +0200

    Linting errors
    More linting errors fixed

--- src/components/server/server.component.jsx
+++ src/components/server/server.component.jsx
@@ -13,8 +13,8 @@ class Server extends React.Component {
         const { json1, json2, json3 } = data;
 
         this.state = {
-        randomJSON: {},
-        json: [json1, json2, json3],
+            randomJSON: {},
+            json: [json1, json2, json3],
         };
     }
 
@@ -22,8 +22,8 @@ class Server extends React.Component {
     getARandom() {
         const state = this.state;
         return (
-        state.json[Math.floor(Math.random() * state.json.length)] &&
-        console.log(state.randomJSON)
+            state.json[Math.floor(Math.random() * state.json.length)] &&
+            console.log(state.randomJSON)
         );
     }
 
@@ -31,9 +31,10 @@ class Server extends React.Component {
         const state = this.state;
         return state.json[Math.floor(Math.random() * state.json.length)];
     }
+
     setJSON() {
         const state = this.state;
-        this.setState({
+            this.setState({
         randomJSON: [this.randomJSON()],
         });
         console.log(state.randomJSON);
@@ -45,148 +46,148 @@ class Server extends React.Component {
 
     render() {
         return (
-        <div className="server">
-            <div className="server-panel">
-            <div className="frontpanel-container">
-                <img
-                className="frontpanel"
-                src={require("../../assets/440_frontpanel.png")}
-                alt="server front panel"
-                />
-                <div
-                className={`status-blink psu1 ${
-                    this.state.randomJSON[0].psu1.status === "ok" ? "green" : "red"
-                }`}
-                >
+            <div className="server">
+                <div className="server-panel">
+                <div className="frontpanel-container">
+                    <img
+                    className="frontpanel"
+                    src={require("../../assets/440_frontpanel.png")}
+                    alt="server front panel"
+                    />
+                    <div
+                    className={`status-blink psu1 ${
+                        this.state.randomJSON[0].psu1.status === "ok" ? "green" : "red"
+                    }`}
+                    >
 
-                </div>
-                <div
-                className={`status-blink psu2 ${
-                    this.state.randomJSON[0].psu2.status === "ok" ? "green" : "red"
-                }`}
-                ></div>
-                <div
-                className={`status-box psu1 ${
-                    this.state.randomJSON[0].psu1.status === "ok" ? "green" : "red"
-                }`}
-                >
+                    </div>
+                    <div
+                    className={`status-blink psu2 ${
+                        this.state.randomJSON[0].psu2.status === "ok" ? "green" : "red"
+                    }`}
+                    ></div>
+                    <div
+                    className={`status-box psu1 ${
+                        this.state.randomJSON[0].psu1.status === "ok" ? "green" : "red"
+                    }`}
+                    >
 
-                </div>
-                <div
-                className={`status-box psu2 ${
-                    this.state.randomJSON[0].psu2.status === "ok" ? "green" : "red"
-                }`}
-                >
+                    </div>
+                    <div
+                    className={`status-box psu2 ${
+                        this.state.randomJSON[0].psu2.status === "ok" ? "green" : "red"
+                    }`}
+                    >
 
+                    </div>
+                    <div
+                    className={`status-box mgmt1 ${
+                        this.state.randomJSON[0].mgmt1.link_status === "no link"
+                        ? "gray"
+                        : "green"
+                    }`}
+                    >
+                    
+                    </div>
+                    <div
+                    className={`status-box mgmt2 ${
+                        this.state.randomJSON[0].mgmt2.link_status === "no link"
+                        ? "gray"
+                        : "green"
+                    }`}
+                    >
+                    
+                    </div>
+                    <div
+                    className={`status-box data1 ${
+                        this.state.randomJSON[0].data1.link_status === "no link"
+                        ? "gray"
+                        : "green"
+                    }`}
+                    >
+                    
+                    </div>
+                    <div
+                    className={`status-box data2 ${
+                        this.state.randomJSON[0].data2.link_status === "no link"
+                        ? "gray"
+                        : "green"
+                    }`}
+                    >
+                    
+                    </div>
                 </div>
-                <div
-                className={`status-box mgmt1 ${
-                    this.state.randomJSON[0].mgmt1.link_status === "no link"
-                    ? "gray"
-                    : "green"
-                }`}
-                >
-                
+                <div className="grid">
+                    <div>
+                    <p>PSU 1</p>
+                    <p>Status: {this.state.randomJSON[0].psu1.status}</p>
+                    <p>
+                        {this.state.randomJSON[0].psu1.status === "ok"
+                        ? this.state.randomJSON[0].psu1.volt + "V"
+                        : "Power Supply AC lost"}
+                    </p>
+                    </div>
+                    <div>
+                    <p>PSU 2</p>
+                    <p>Status: {this.state.randomJSON[0].psu2.status}</p>
+                    <p>
+                        {this.state.randomJSON[0].psu2.status === "ok"
+                        ? this.state.randomJSON[0].psu2.volt + "V"
+                        : "Power Supply AC lost"}
+                    </p>
+                    </div>
+                    <div className="mgmt">
+                    <table>
+                        <thead>
+                        <tr>
+                            <td>MGMT</td>
+                            <td>Status</td>
+                            <td>BPS</td>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <tr>
+                            <td>1</td>
+                            <td>{this.state.randomJSON[0].mgmt1.link_status}</td>
+                            <td>{this.state.randomJSON[0].mgmt1.bps}</td>
+                        </tr>
+                        <tr>
+                            <td>2</td>
+                            <td>{this.state.randomJSON[0].mgmt2.link_status}</td>
+                            <td>{this.state.randomJSON[0].mgmt2.bps}</td>
+                        </tr>
+                        </tbody>
+                    </table>
+                    </div>
+                    <div className="data">
+                    <table>
+                        <thead>
+                        <tr>
+                            <td>Data</td>
+                            <td>Status</td>
+                            <td>BPS</td>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <tr>
+                            <td>1</td>
+                            <td>{this.state.randomJSON[0].data1.link_status}</td>
+                            <td>{this.state.randomJSON[0].data1.bps}</td>
+                        </tr>
+                        <tr>
+                            <td>2</td>
+                            <td>{this.state.randomJSON[0].data2.link_status}</td>
+                            <td>{this.state.randomJSON[0].data2.bps}</td>
+                        </tr>
+                        </tbody>
+                    </table>
+                    </div>
                 </div>
-                <div
-                className={`status-box mgmt2 ${
-                    this.state.randomJSON[0].mgmt2.link_status === "no link"
-                    ? "gray"
-                    : "green"
-                }`}
-                >
-                
                 </div>
-                <div
-                className={`status-box data1 ${
-                    this.state.randomJSON[0].data1.link_status === "no link"
-                    ? "gray"
-                    : "green"
-                }`}
-                >
-                
-                </div>
-                <div
-                className={`status-box data2 ${
-                    this.state.randomJSON[0].data2.link_status === "no link"
-                    ? "gray"
-                    : "green"
-                }`}
-                >
-                
+                <div className="get-random-json-button" onClick={() => this.setJSON()}>
+                Get Random JSON
                 </div>
             </div>
-            <div className="grid">
-                <div>
-                <p>PSU 1</p>
-                <p>Status: {this.state.randomJSON[0].psu1.status}</p>
-                <p>
-                    {this.state.randomJSON[0].psu1.status === "ok"
-                    ? this.state.randomJSON[0].psu1.volt + "V"
-                    : "Power Supply AC lost"}
-                </p>
-                </div>
-                <div>
-                <p>PSU 2</p>
-                <p>Status: {this.state.randomJSON[0].psu2.status}</p>
-                <p>
-                    {this.state.randomJSON[0].psu2.status === "ok"
-                    ? this.state.randomJSON[0].psu2.volt + "V"
-                    : "Power Supply AC lost"}
-                </p>
-                </div>
-                <div className="mgmt">
-                <table>
-                    <thead>
-                    <tr>
-                        <td>MGMT</td>
-                        <td>Status</td>
-                        <td>BPS</td>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    <tr>
-                        <td>1</td>
-                        <td>{this.state.randomJSON[0].mgmt1.link_status}</td>
-                        <td>{this.state.randomJSON[0].mgmt1.bps}</td>
-                    </tr>
-                    <tr>
-                        <td>2</td>
-                        <td>{this.state.randomJSON[0].mgmt2.link_status}</td>
-                        <td>{this.state.randomJSON[0].mgmt2.bps}</td>
-                    </tr>
-                    </tbody>
-                </table>
-                </div>
-                <div className="data">
-                <table>
-                    <thead>
-                    <tr>
-                        <td>Data</td>
-                        <td>Status</td>
-                        <td>BPS</td>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    <tr>
-                        <td>1</td>
-                        <td>{this.state.randomJSON[0].data1.link_status}</td>
-                        <td>{this.state.randomJSON[0].data1.bps}</td>
-                    </tr>
-                    <tr>
-                        <td>2</td>
-                        <td>{this.state.randomJSON[0].data2.link_status}</td>
-                        <td>{this.state.randomJSON[0].data2.bps}</td>
-                    </tr>
-                    </tbody>
-                </table>
-                </div>
-            </div>
-            </div>
-            <div className="get-random-json-button" onClick={() => this.setJSON()}>
-            Get Random JSON
-            </div>
-        </div>
         );
     }
 }
